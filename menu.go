19649a9d78b3045a216593ae990384a643e7f43a
Arne Roomann-Kurrik <kurrik@gmail.com>
2014-04-08 22:22:50 -0700

    Menu refactor allowing menu items to be bound to memory locations.

--- menu.go
+++ menu.go
@@ -18,106 +18,197 @@ import (
 	"fmt"
 )
 
+type MenuItem interface {
+	Highlighted() bool
+	Label() string
+	setHighlighted(val bool)
+	Parent() MenuItem
+	setParent(item MenuItem)
+	Active() bool
+	setActive(val bool)
+}
+
+type baseMenuItem struct {
+	label       string
+	highlighted bool
+	active      bool
+	parent      MenuItem
+}
+
+func (mi *baseMenuItem) Label() string {
+	return mi.label
+}
+
+func (mi *baseMenuItem) Highlighted() bool {
+	return mi.highlighted
+}
+
+func (mi *baseMenuItem) setHighlighted(val bool) {
+	mi.highlighted = val
+}
+
+func (mi *baseMenuItem) Active() bool {
+	return mi.active
+}
+
+func (mi *baseMenuItem) setActive(val bool) {
+	mi.active = val
+}
+
+func (mi *baseMenuItem) Parent() MenuItem {
+	return mi.parent
+}
+
+func (mi *baseMenuItem) setParent(item MenuItem) {
+	mi.parent = item
+}
+
+type BackMenuItem struct {
+	baseMenuItem
+}
+
+func NewBackMenuItem(label string) (item *BackMenuItem) {
+	item = &BackMenuItem{}
+	item.baseMenuItem.label = label
+	return
+}
+
+type ParentMenuItem struct {
+	baseMenuItem
+	children []MenuItem
+}
+
+func NewParentMenuItem(label string, children []MenuItem) (item *ParentMenuItem) {
+	item = &ParentMenuItem{
+		baseMenuItem: baseMenuItem{
+			label: label,
+		},
+		children: children,
+	}
+	for _, child := range item.children {
+		child.setParent(item)
+	}
+	return
+}
+
+func (mi *ParentMenuItem) Children() []MenuItem {
+	return mi.children
+}
+
 type MenuItemData struct {
 	Key   int32
 	Value int32
 }
 
-type MenuNode struct {
-	data        *MenuItemData
-	label       string
-	parent      *MenuNode
-	children    []*MenuNode
-	highlighted bool
-	back        bool
+type KeyValueMenuItem struct {
+	baseMenuItem
+	data *MenuItemData
 }
 
-func (mn *MenuNode) IsHighlighted() bool {
-	return mn.highlighted
+func NewKeyValueMenuItem(label string, key, value int32) *KeyValueMenuItem {
+	return &KeyValueMenuItem{
+		baseMenuItem: baseMenuItem{label: label},
+		data: &MenuItemData{
+			Key:   key,
+			Value: value,
+		},
+	}
 }
 
-func (mn *MenuNode) Label() string {
-	return mn.label
+func (mi *KeyValueMenuItem) Data() *MenuItemData {
+	return mi.data
+}
+
+type BoundValueMenuItem struct {
+	KeyValueMenuItem
+	dest *int32
+}
+
+func NewBoundValueMenuItem(label string, val int32, dest *int32) *BoundValueMenuItem {
+	return &BoundValueMenuItem{
+		KeyValueMenuItem: *NewKeyValueMenuItem(label, -1, val),
+		dest:             dest,
+	}
+}
+
+func (mi *BoundValueMenuItem) IsSameAsDest() bool {
+	return *(mi.dest) == mi.KeyValueMenuItem.data.Value
+}
+
+func (mi *BoundValueMenuItem) SetDest() {
+	*(mi.dest) = mi.KeyValueMenuItem.data.Value
 }
 
 type Menu struct {
-	root        *MenuNode
+	root        MenuItem
+	items       []MenuItem
 	highlighted int
 }
 
-func NewMenuNode(key int32, val int32, label string, children []*MenuNode) *MenuNode {
-	var node = &MenuNode{
-		data:        &MenuItemData{Key: key, Value: val},
-		label:       label,
-		children:    children,
-		back:        false,
-		highlighted: false,
-	}
-	for _, child := range children {
-		child.parent = node
-	}
-	return node
-}
-
-func BackMenuNode(label string) *MenuNode {
-	return &MenuNode{
-		data:        nil,
-		label:       label,
-		parent:      nil,
-		children:    nil,
-		highlighted: false,
-		back:        true,
-	}
-}
-
-func NewMenu(items []*MenuNode) (menu *Menu, err error) {
+func NewMenu(items []MenuItem) (menu *Menu, err error) {
 	if len(items) == 0 {
 		err = fmt.Errorf("No items in menu")
 		return
 	}
 	menu = &Menu{
-		root: NewMenuNode(0, 0, "", items),
+		root:  NewParentMenuItem("root", items),
+		items: items,
 	}
 	menu.updateHighlighted(0)
 	return
 }
 
-func (m *Menu) getHighlighted() *MenuNode {
-	return m.root.children[m.highlighted%len(m.root.children)]
+func (m *Menu) getHighlighted() MenuItem {
+	return m.items[m.highlighted%len(m.items)]
 }
 
-func (m *Menu) Items() []*MenuNode {
-	return m.root.children
+func (m *Menu) Items() []MenuItem {
+	return m.items
+}
+
+func (m *Menu) selectItem(item MenuItem) *MenuItemData {
+	if item == nil {
+		return nil
+	}
+	switch selected := item.(type) {
+	case *BackMenuItem:
+		m.selectItem(selected.Parent().Parent())
+	case *ParentMenuItem:
+		m.items = selected.Children()
+		m.updateHighlighted(0)
+	case *KeyValueMenuItem:
+		return selected.Data()
+	case *BoundValueMenuItem:
+		selected.SetDest()
+		m.updateHighlighted(m.highlighted)
+	}
+	return nil
 }
 
 func (m *Menu) Select() *MenuItemData {
-	h := m.getHighlighted()
-	if len(h.children) != 0 {
-		m.root = h
-		m.updateHighlighted(0)
-		return nil
-	} else if h.back == true && h.parent.parent != nil {
-		m.root = h.parent.parent
-		m.updateHighlighted(0)
-		return nil
-	}
-	return h.data
+	return m.selectItem(m.getHighlighted())
 }
 
 func (m *Menu) Next() {
-	m.updateHighlighted((m.highlighted + 1) % len(m.root.children))
+	m.updateHighlighted((m.highlighted + 1) % len(m.items))
 }
 
 func (m *Menu) Prev() {
-	count := len(m.root.children)
+	count := len(m.items)
 	m.updateHighlighted((m.highlighted + count - 1) % count)
 }
 
 func (m *Menu) updateHighlighted(i int) {
-	if len(m.root.children) <= i {
+	for _, item := range m.items {
+		switch curr := item.(type) {
+		case *BoundValueMenuItem:
+			item.setActive(curr.IsSameAsDest())
+		}
+	}
+	if len(m.items) <= i {
 		return
 	}
-	m.root.children[m.highlighted].highlighted = false
+	m.items[m.highlighted].setHighlighted(false)
 	m.highlighted = i
-	m.root.children[i].highlighted = true
+	m.items[i].setHighlighted(true)
 }
