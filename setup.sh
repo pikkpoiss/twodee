52743abfdf5b4011efe68e23c9bfb939f82d5df8
Arne Roomann-Kurrik <kurrik@gmail.com>
2015-04-04 11:33:46 -0700

    Include setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+go get -u code.google.com/p/freetype-go/freetype
+go get -u code.google.com/p/freetype-go/freetype/raster
+go get -u code.google.com/p/freetype-go/freetype/truetype
+go get -u github.com/Agon/googlmath
+go get -u github.com/go-gl/gl/v3.2-core/gl
+go get -u github.com/go-gl/glfw/v3.1/glfw
+go get -u github.com/kurrik/Go-SDL/mixer
+go get -u github.com/kurrik/Go-SDL/sdl
