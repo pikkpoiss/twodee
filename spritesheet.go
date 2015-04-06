e283b531ba412d167d29857b1f9aeb65c35e9c15
Arne Roomann-Kurrik <kurrik@gmail.com>
2015-04-05 23:32:38 -0700

    Initial work on spritesheet parsing, using TexturePacker format as a reference implementation

---
+++ spritesheet.go
@@ -0,0 +1,155 @@
+// Copyright 2015 Arne Roomann-Kurrik
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package twodee
+
+import (
+	"encoding/json"
+)
+
+type SpritesheetPoint struct {
+	X        float32
+	Y        float32
+	TextureX float32
+	TextureY float32
+}
+
+type SpritesheetFrame struct {
+	Points []SpritesheetPoint
+}
+
+type SpritesheetFrameConfig struct {
+	sourceX          float32
+	sourceY          float32
+	sourceW          float32
+	sourceH          float32
+	originalW        float32
+	originalH        float32
+	textureX         float32
+	textureY         float32
+	textureW         float32
+	textureH         float32
+	textureOriginalW float32
+	textureOriginalH float32
+	pivotX           float32
+	pivotY           float32
+}
+
+func (c SpritesheetFrameConfig) ToSpritesheetFrame() *SpritesheetFrame {
+	var (
+		x    = c.sourceX/c.originalW - c.pivotX
+		y    = c.sourceY/c.originalH - c.pivotY
+		w    = c.sourceW / c.originalW
+		h    = c.sourceH / c.originalH
+		texX = c.textureX / c.textureOriginalW
+		texY = c.textureY / c.textureOriginalH
+		texW = c.textureW / c.textureOriginalW
+		texH = c.textureH / c.textureOriginalH
+	)
+	return &SpritesheetFrame{
+		Points: []SpritesheetPoint{
+			SpritesheetPoint{x, y, texX, texY},
+			SpritesheetPoint{x, y + h, texX, texY + texH},
+			SpritesheetPoint{x + w, y, texX + texW, texY},
+			SpritesheetPoint{x + w, y + h, texX + texW, texY + texH},
+		},
+	}
+}
+
+type Spritesheet struct {
+	frames map[string]*SpritesheetFrame
+}
+
+func NewSpritesheet() *Spritesheet {
+	return &Spritesheet{
+		frames: map[string]*SpritesheetFrame{},
+	}
+}
+
+func (s *Spritesheet) GetFrame(name string) *SpritesheetFrame {
+	var (
+		present bool
+	)
+	if _, present = s.frames[name]; !present {
+		return nil
+	}
+	return s.frames[name]
+}
+
+func (s *Spritesheet) AddFrame(name string, config SpritesheetFrameConfig) {
+	s.frames[name] = config.ToSpritesheetFrame()
+}
+
+type texturePackerFloatCoords struct {
+	X float32 `json:x,omitempty`
+	Y float32 `json:y,omitempty`
+}
+
+type texturePackerIntCoords struct {
+	X int `json:x,omitempty`
+	Y int `json:y,omitempty`
+	W int `json:w,omitempty`
+	H int `json:h,omitempty`
+}
+
+type texturePackerFrame struct {
+	Filename         string                   `json:filename`
+	Frame            texturePackerIntCoords   `json:frame`
+	Rotated          bool                     `json:rotated`
+	Trimmed          bool                     `json:trimmed`
+	SpriteSourceSize texturePackerIntCoords   `json:spriteSourceSize`
+	SourceSize       texturePackerIntCoords   `json:sourceSize`
+	Pivot            texturePackerFloatCoords `json:pivot`
+}
+
+type texturePackerMeta struct {
+	Image  string                 `json:image`
+	Format string                 `json:format`
+	Size   texturePackerIntCoords `json:size`
+	Scale  string                 `json:scale`
+}
+
+type texturePackerJSONArray struct {
+	Frames []texturePackerFrame `json:frames`
+	Meta   texturePackerMeta    `json:meta`
+}
+
+func ParseTexturePackerJSONArrayString(contents string) (s *Spritesheet, err error) {
+	var (
+		parsed texturePackerJSONArray
+	)
+	if err = json.Unmarshal([]byte(contents), &parsed); err != nil {
+		return
+	}
+	s = NewSpritesheet()
+	for _, frame := range parsed.Frames {
+		s.AddFrame(frame.Filename, SpritesheetFrameConfig{
+			sourceX:          float32(frame.SpriteSourceSize.X),
+			sourceY:          float32(frame.SpriteSourceSize.Y),
+			sourceW:          float32(frame.SpriteSourceSize.W),
+			sourceH:          float32(frame.SpriteSourceSize.H),
+			originalW:        float32(frame.SourceSize.W),
+			originalH:        float32(frame.SourceSize.H),
+			textureX:         float32(frame.Frame.X),
+			textureY:         float32(frame.Frame.Y),
+			textureW:         float32(frame.Frame.W),
+			textureH:         float32(frame.Frame.H),
+			textureOriginalW: float32(parsed.Meta.Size.W),
+			textureOriginalH: float32(parsed.Meta.Size.H),
+			pivotX:           frame.Pivot.X,
+			pivotY:           frame.Pivot.Y,
+		})
+	}
+	return
+}
---
+++ spritesheet_test.go
@@ -0,0 +1,84 @@
+// Copyright 2015 Arne Roomann-Kurrik
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package twodee
+
+import (
+	"testing"
+)
+
+const TEST_ARRAY_STRING = `{
+  "frames": [
+    {
+      "filename": "frame01.png",
+      "frame": {"x":2,"y":112,"w":26,"h":26},
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {"x":3,"y":3,"w":26,"h":26},
+      "sourceSize": {"w":32,"h":32},
+      "pivot": {"x":0.5,"y":0.5}
+    },
+    {
+      "filename": "frame02.png",
+      "frame": {"x":2,"y":2,"w":30,"h":30},
+      "rotated": false,
+      "trimmed": true,
+      "spriteSourceSize": {"x":1,"y":1,"w":30,"h":30},
+      "sourceSize": {"w":32,"h":32},
+      "pivot": {"x":0.5,"y":0.5}
+    }
+  ],
+  "meta": {
+    "app": "http://www.codeandweb.com/texturepacker",
+    "version": "1.0",
+    "image": "test.png",
+    "format": "RGBA8888",
+    "size": {"w":64,"h":271},
+    "scale": "1",
+    "smartupdate": "$TexturePacker:SmartUpdate:xxxx"
+  }
+}`
+
+func TestParseTexturePackerJSONArrayString(t *testing.T) {
+	var (
+		sheet  *Spritesheet
+		frame  *SpritesheetFrame
+		err    error
+		point0 = SpritesheetPoint{-0.40625, -0.40625, 0.03125, 0.41328412}
+		point1 = SpritesheetPoint{-0.40625, 0.40625, 0.03125, 0.5092251}
+		point2 = SpritesheetPoint{0.40625, -0.40625, 0.4375, 0.41328412}
+		point3 = SpritesheetPoint{0.40625, 0.40625, 0.4375, 0.5092251}
+	)
+	if sheet, err = ParseTexturePackerJSONArrayString(TEST_ARRAY_STRING); err != nil {
+		t.Fatalf("Problem parsing JSON array: %v", err)
+	}
+	if sheet.GetFrame("non_existent_frame.png") != nil {
+		t.Fatalf("GetFrame with unknown name should return nil")
+	}
+	if frame = sheet.GetFrame("frame01.png"); frame == nil {
+		t.Fatalf("GetFrame with known name should return non-nil")
+	}
+	if frame.Points[0] != point0 {
+		t.Fatalf("Invalid point, got %v, expected %v", frame.Points[0], point0)
+	}
+	if frame.Points[1] != point1 {
+		t.Fatalf("Invalid point, got %v, expected %v", frame.Points[1], point1)
+	}
+	if frame.Points[2] != point2 {
+		t.Fatalf("Invalid point, got %v, expected %v", frame.Points[2], point2)
+	}
+	if frame.Points[3] != point3 {
+		t.Fatalf("Invalid point, got %v, expected %v", frame.Points[3], point3)
+	}
+}
