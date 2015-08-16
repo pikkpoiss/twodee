d74c39889280205cd8710ef21b14d74cac2d610f
Arne Roomann-Kurrik <kurrik@gmail.com>
2015-08-15 22:21:05 -0700

    SDL

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,6 +12,9 @@ green "INIT" "Prefix is $PREFIX"
 if [ -n "$CLEAN" ]; then
   yellow "CLEAN" "Deleting the build path"
   rm -rf $BUILDROOT
+  yellow "CLEAN" "Deleting $GOPATH/src and $GOPATH/pkg"
+  rm -rf $GOPATH/src
+  rm -rf $GOPATH/pkg
 fi
 
 mkdir -p $BUILDROOT
@@ -20,10 +23,11 @@ cd $BUILDROOT
 
 ##### Libraries ################################################################
 
-if file_exists $BUILDROOT/foo; then
+if file_exists $PREFIX/lib/libglfw3.a; then
   green "EXISTS" "glfw"
 else
   yellow "BUILD" "glfw"
+  rm -rf glfw-3.1.1
   unzip glfw-3.1.1.zip
   cd glfw-3.1.1
   cmake \
@@ -32,4 +36,65 @@ else
     .
   make
   make install
+  cd ..
 fi
+
+if file_exists $PREFIX/lib/libogg.a; then
+  green "EXISTS" "ogg"
+else
+  yellow "BUILD" "ogg"
+  rm -rf libogg-1.3.2
+  unzip libogg-1.3.2.zip
+  cd libogg-1.3.2
+  ./configure \
+    --prefix=$PREFIX \
+    --disable-shared
+  make
+  make install
+  cd ..
+fi
+
+if file_exists $PREFIX/lib/libvorbis.a; then
+  green "EXISTS" "vorbis"
+else
+  yellow "BUILD" "vorbis"
+  rm -rf libvorbis-1.3.5
+  unzip libvorbis-1.3.5.zip
+  cd libvorbis-1.3.5
+  ./configure \
+    --prefix=$PREFIX \
+    --disable-shared
+  make
+  make install
+  cd ..
+fi
+
+if file_exists $PREFIX/lib/libSDL2.a; then
+  green "EXISTS" "SDL2"
+else
+  yellow "BUILD" "SDL2"
+  rm -rf SDL2-2.0.3
+  unzip SDL2-2.0.3.zip
+  cd SDL2-2.0.3
+  ./configure \
+    --prefix=$PREFIX \
+    --disable-shared
+  make
+  make install
+  cd ..
+fi
+
+
+##### Go libraries #############################################################
+
+CGO_CFLAGS="-I$PREFIX/include"
+
+go get -u code.google.com/p/freetype-go/freetype
+go get -u code.google.com/p/freetype-go/freetype/raster
+go get -u code.google.com/p/freetype-go/freetype/truetype
+go get -u github.com/go-gl/gl/v3.3-core/gl
+go get -u github.com/go-gl/glfw/v3.1/glfw
+go get -u github.com/go-gl/mathgl/mgl32
+go get -u github.com/kurrik/Go-SDL/mixer
+go get -u github.com/kurrik/Go-SDL/sdl
+go get -u github.com/robertkrimen/otto
